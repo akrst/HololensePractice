ad7dad887e6b7ec476ca8579506cb19954ede907
1080TI\akira <akirasato@psychic-vr-lab.com>
2018-06-04 12:17:49 +0900

    半沢さん関連のBody調整

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+    public string SceneName001;
+    public string SceneName002;
+    public string SceneName003;
+    public string SceneName004;
+    public string SceneName005;
+    
+	public void SceneStartButton001(){
+        SceneManager.LoadScene(SceneName001);
+	}
+
+    public void SceneStartButton002()
+    {
+        SceneManager.LoadScene(SceneName002);
+    }
+
+    public void SceneStartButton003()
+    {
+        SceneManager.LoadScene(SceneName003);
+    }
+
+    public void SceneStartButton004()
+    {
+        SceneManager.LoadScene(SceneName004);
+    }
+
+    public void SceneStartButton005()
+    {
+        SceneManager.LoadScene(SceneName005);
+    }
+
+}
